b5f5514756da9b2ddbe132d1cc82bd968631a205
maquendi <mbn04251989@gmail.com>
2024-11-07 10:37:38 -0400

    updated mi

--- ui-smartlib-web/src/config/drizzle/migrations/0000_cute_marvel_zombies.sql
+++ ui-smartlib-web/src/config/drizzle/migrations/0000_cute_marvel_zombies.sql
@@ -1,3 +1,11 @@
+CREATE TABLE `book_category` (
+	`id` text PRIMARY KEY NOT NULL,
+	`book_id` text,
+	`category_id` text,
+	FOREIGN KEY (`book_id`) REFERENCES `books`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`category_id`) REFERENCES `category`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
 CREATE TABLE `books_copies` (
 	`id` text PRIMARY KEY NOT NULL,
 	`book_id` text NOT NULL,
@@ -7,7 +15,8 @@ CREATE TABLE `books_copies` (
 	`created_at` text DEFAULT (current_timestamp) NOT NULL,
 	`updated_at` text DEFAULT (current_timestamp) NOT NULL,
 	FOREIGN KEY (`book_id`) REFERENCES `books`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`isbn`) REFERENCES `books_edition`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`isbn`) REFERENCES `books_edition`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`at_position`) REFERENCES `books`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `books_edition` (
@@ -19,7 +28,7 @@ CREATE TABLE `books_images` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`url` text NOT NULL,
 	`book_id` text NOT NULL,
-	`isbn` text NOT NULL,
+	`isbn` text,
 	`isSincronized` integer DEFAULT false
 );
 --> statement-breakpoint
@@ -52,10 +61,11 @@ CREATE TABLE `book_loan` (
 CREATE TABLE `books` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
-	`price` integer NOT NULL,
+	`isbn` text,
 	`author` text NOT NULL,
 	`created_at` text DEFAULT (current_timestamp) NOT NULL,
-	`updated_at` text DEFAULT (current_timestamp) NOT NULL
+	`updated_at` text DEFAULT (current_timestamp) NOT NULL,
+	FOREIGN KEY (`isbn`) REFERENCES `books_edition`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `cart_item` (
@@ -68,8 +78,6 @@ CREATE TABLE `cart_item` (
 	FOREIGN KEY (`cart_id`) REFERENCES `cart`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-CREATE INDEX `cart_idx` ON `cart_item` (`cart_id`);--> statement-breakpoint
-CREATE UNIQUE INDEX `cart_item_cart_id_isbn_unique` ON `cart_item` (`cart_id`,`isbn`);--> statement-breakpoint
 CREATE TABLE `cart` (
 	`id` text PRIMARY KEY NOT NULL,
 	`user_id` text NOT NULL,
@@ -77,6 +85,11 @@ CREATE TABLE `cart` (
 	`created_at` text DEFAULT (current_timestamp),
 	`updated_at` text DEFAULT (current_timestamp),
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `category` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text
 );
 --> statement-breakpoint
 CREATE TABLE `user` (
