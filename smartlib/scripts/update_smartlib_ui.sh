a49f9bd2cec91e9dd438b46abe0a361cf79040ea
maquendi <mbn04251989@gmail.com>
2025-03-30 20:23:31 -0400

    update

--- smartlib/scripts/update_smartlib_ui.sh
+++ smartlib/scripts/update_smartlib_ui.sh
@@ -23,7 +23,6 @@ cd "$REPO_PATH" || { echo "Failed to cd into repository"; exit 1; }
 echo "Pulling latest changes from master branch..."
 git checkout smartlib/master && git pull origin master || { echo "Git pull failed"; exit 1; }
 
-git commit -m "Auto commit before stash" || { echo "Git commit failed"; exit 1; }
 
 # Navigate to the UI directory
 cd "$UI_PATH" || { echo "Failed to cd into UI directory"; exit 1; }
@@ -35,6 +34,8 @@ echo "Building the UI..."
 npm run build || { echo "npm build failed"; exit 1; }
 
 echo "Update and build completed successfully!"
+
+git commit -m "Auto commit before stash" || { echo "Git commit failed"; exit 1; }
 
 # restart the pm2 process
 echo "Restarting pm2 smartlib-ui process..."
